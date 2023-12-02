d3d0dc88f0a46c5ea566001ecb45ed7efe89a7e4
uchesachin96@gmail.com <uchesachin96@gmail.com>
2023-12-02 14:31:53 +0530

    changes

--- src/test/java/com/jenkinsproject002/jenkinTest002.java
+++ src/test/java/com/jenkinsproject002/jenkinTest002.java
@@ -1,7 +1,10 @@
 package com.jenkinsproject002;
 
+import org.testng.annotations.Test;
+
 public class jenkinTest002 {
 
+	@Test
 	public void jenkinmethodTest()
 	{
 		System.out.println("hi iam jenkin 02");
